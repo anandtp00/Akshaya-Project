d697192bd0370a5f7ea24ce1bdc0f5c7056757f5
anandtp00 <anandtp00@gmail.com>
2020-05-13 22:04:43 +0530

    body component and admin component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponentComponent } from './body-component/body-component.component';
+import { AdminComponentComponent } from './admin-component/admin-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponentComponent,
+    AdminComponentComponent
   ],
   imports: [
     BrowserModule,
