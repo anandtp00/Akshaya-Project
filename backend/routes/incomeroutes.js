87ea84fa6931819659c40d282e1a9306f4eaa8cc
anandtp00 <anandtp00@gmail.com>
2020-05-15 23:02:52 +0530

    configurated the income router file

--- backend/routes/incomeroutes.js
+++ backend/routes/incomeroutes.js
@@ -1,5 +1,6 @@
 const router=require('express').Router();
 
+//calling the required controller
 const incomecontroller=require('../controllers/incomecontroller');
 
 router.post('/add',incomecontroller.addincome);
