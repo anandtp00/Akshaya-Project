2cb70f360c035145fd5f1129219c6c0dd0d94dc4
anandtp00 <anandtp00@gmail.com>
2020-05-15 23:00:22 +0530

    configurating the app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
+//modules requiring
 const express=require('express');
 const bodyparser=require('body-parser');
 const cors=require('cors');
 const morgan=require('morgan');
 
-
+//initilization of app
 const app=express();
 
-
+//database connection calling
 require('./dbconfig/dbconfiguration');
 
-
+//routers accessing
 const incrouter=require('./routes/incomeroutes')
 
 // var datetime = new Date();
@@ -17,16 +18,16 @@ const incrouter=require('./routes/incomeroutes')
 // console.log(date);
 
 
-
+//using required modules
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(cors({origin:'http://localhost:4200'}));
 
-
+//connecting to router
 app.use('/income',incrouter)
 
-
+//server listening to the port given
 const port=process.env.PORT || 4000
 
 app.listen(port, (error)=>{
