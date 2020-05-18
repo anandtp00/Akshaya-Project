1e49e1624c60b8964f2e5b3379c9b283c7dc1a02
anandtp00 <anandtp00@gmail.com>
2020-05-18 16:48:57 +0530

    frontend of statement component made working

--- frontend/src/app/layout-component/statement/statement.component.ts
+++ frontend/src/app/layout-component/statement/statement.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+import { StatementService } from '../../shared/statement.service';
+import { Statement } from '../../shared/statement.model';
+import { NgForm } from '@angular/forms';
+import { element } from 'protractor';
+
 
 @Component({
   selector: 'app-statement',
   templateUrl: './statement.component.html',
-  styleUrls: ['./statement.component.css']
+  styleUrls: ['./statement.component.css'],
+  providers: [StatementService]
 })
 export class StatementComponent implements OnInit {
 
-  constructor() { }
+  constructor(public statementservice: StatementService, private router: Router) { }
 
   ngOnInit(): void {
+    this.todaysStatement();
+  }
+
+  todaysStatement() {
+    this.statementservice.getTodaysStatement().subscribe((response) => {
+      this.statementservice.statements = response as Statement[];
+      let date_ob = new Date(response[0].date);
+      let date = date_ob.getDate();
+      let month = date_ob.getMonth() + 1;
+      let year = date_ob.getFullYear();
+      const today = date + "-" + month + "-" + year;
+      this.statementservice.today=today
+
+      let sum1=0;
+      let sum2=0;
+
+
+      this.statementservice.statements.forEach((i)=>{
+        let inc = i.income;
+        if(inc === undefined){
+          inc = 0;
+        }
+        sum1=sum1+inc;
+      })
+
+      this.statementservice.tIncome=sum1;
+
+      this.statementservice.statements.forEach((i)=>{
+        let exp = i.expense;
+        if(exp === undefined){
+          exp = 0;
+        }
+        sum2=sum2+exp;
+      })
+
+      this.statementservice.tExpense=sum2;
+     
+    });
   }
 
 }
--- frontend/src/app/shared/statement.service.ts
+++ frontend/src/app/shared/statement.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+import {  Statement } from './statement.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class StatementService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  statements: Statement[];
+
+  today : string;
+
+  tIncome : Number;
+  tExpense : Number;
+
+  getTodaysStatement(){
+    return this.http.get(environment.statementbaseURL+'/todaysstatement')
+  }
 }
