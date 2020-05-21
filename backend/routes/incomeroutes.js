9494f5ad3a75d7f6a0a70bb4b7712126bf2063f0
anandtp00 <anandtp00@gmail.com>
2020-05-22 02:59:55 +0530

    backend routes for income update and delete

--- backend/routes/incomeroutes.js
+++ backend/routes/incomeroutes.js
@@ -6,7 +6,7 @@ const incomecontroller=require('../controllers/incomecontroller');
 router.post('/add',incomecontroller.addIncome);
 router.get('/gettodaysincome',incomecontroller.getTodayIncome);
 
-router.get('/updateincome/:_id',incomecontroller.updateIncomeData);
+router.put('/updateincome/:id',incomecontroller.updateIncomeData);
 router.get('/deleteincome/:_id',incomecontroller.deleteIncomeData);
 
 
