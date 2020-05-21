7ecfc0313bceed40b9e577862e6328d8eb17a4ee
anandtp00 <anandtp00@gmail.com>
2020-05-22 03:00:58 +0530

    statement controller touch up

--- backend/routes/statementrouter.js
+++ backend/routes/statementrouter.js
@@ -3,15 +3,6 @@ const router=require('express').Router();
 //calling the required controller
 const statementcontroller=require('../controllers/statementcontroller');
 
-
 router.get('/todaysstatement',statementcontroller.getTodaysStatement);
-
-
-
-
-
-
-
-
 
 module.exports=router;
