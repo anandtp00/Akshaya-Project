31bc7405c3db310bbc1542ae73c363c97e132c60
anandtp00 <anandtp00@gmail.com>
2020-05-22 02:57:37 +0530

    backend router for expense

--- backend/routes/expenseroutse.js
+++ backend/routes/expenseroutse.js
@@ -6,6 +6,9 @@ const expensecontroller=require('../controllers/expensecontroller');
 router.post('/add',expensecontroller.addExpense);
 router.get('/gettodaysexpense',expensecontroller.getTodayExpense);
 
+router.put('/updateexpense/:id',expensecontroller.updateExpenseData);
+router.get('/deleteexpense/:_id',expensecontroller.deleteExpenseData);
+
 
 
 
