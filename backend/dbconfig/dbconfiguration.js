665f24a9fb96941df000e48f99660a32cbe69efd
anandtp00 <anandtp00@gmail.com>
2020-05-22 01:40:16 +0530

    database configuration

--- backend/dbconfig/dbconfiguration.js
+++ backend/dbconfig/dbconfiguration.js
@@ -5,7 +5,7 @@ const pass=process.env.mongoPass;
 const url= user+':'+pass;
 
 //mongodb cluster connection url : mongodb+srv://akshayahrm:<password>@akshayahrm-nw0kb.mongodb.net/akshayaHRM?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://'+url+'@akshayahrm-nw0kb.mongodb.net/akshayaHRM?retryWrites=true&w=majority',{ useNewUrlParser: true },(error)=>{
+mongoose.connect('mongodb+srv://'+url+'@akshayaHRM-nw0kb.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true },(error)=>{
     if(!error){
         console.log('MongoDB connection achieved');
     }
