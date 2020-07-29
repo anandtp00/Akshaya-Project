1e3c08b058761379f5b921135603fdd251e2131c
anandtp00 <anandtp00@gmail.com>
2020-07-30 01:17:57 +0530

    changes

--- backend/dbconfig/dbconfiguration.js
+++ backend/dbconfig/dbconfiguration.js
@@ -3,7 +3,6 @@ const mongoose=require('mongoose');
 const user=process.env.mongoUser;
 const pass=process.env.mongoPass;
 const url= user+':'+pass;
-
 //mongodb cluster connection url : mongodb+srv://akshayahrm:<password>@akshayahrm-nw0kb.mongodb.net/akshayaHRM?retryWrites=true&w=majority
 mongoose.connect('mongodb+srv://'+url+'@akshayaHRM-nw0kb.mongodb.net/akshayaHRM?retryWrites=true&w=majority',{ useNewUrlParser: true },(error)=>{
     if(!error){
